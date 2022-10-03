ae34224b94d3df6d539be1bfd83db9ad06c4ba22
AlexSolis21 <aaapsolis@gmail.com>
2022-10-02 21:06:33 -0600

    se inicia proyecto

---
+++ app/Http/Livewire/Pages/DatesManagement.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Livewire\Pages;
+
+use App\Models\Dates;
+use App\Models\Patient;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class DatesManagement extends Component
+{
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $fecha_cita, $hora_cita, $descripcion, $patient_id, $citas_id;
+
+    public $search = '';
+
+    protected function rules()
+    {
+        return [
+            'fecha_cita' => 'required|string|min:6',
+            'hora_cita' => 'required|',
+            'descripcion' => 'required|string',
+            'patient_id' => 'required',
+        ];
+    }
+   
+    public function updated($fields)
+    {
+        $this->validateOnly($fields);
+    }
+
+    public function saveCitas()
+    {
+        $validatedData = $this->validate();
+
+        Dates::create($validatedData);
+        session()->flash('message', 'Cita creado correctamente');
+        $this->resetInput();
+        $this->dispatchBrowserEvent('close-modal');
+    }
+    
+    public function editCitas(int $date_id)
+    {
+        $dates = Dates::find($date_id);
+        if ($dates) {
+            $this->dates_id = $dates->id;
+            $this->fecha_cita = $dates->fecha_cita;
+            $this->hora_cita = $dates->hora_cita;
+            $this->descripcion = $dates->descripcion;
+            $this->patient_id = $dates->patient_id;
+        } else {
+            return redirect()->to('/citas');
+        }
+    }
+
+    public function updateCitas()
+    {
+        $validatedData = $this->validate();
+        Dates::where('id', $this->dates_id)->update($validatedData);
+        session()->flash('message', 'Cita Actualizada Correctamente');
+        $this->resetInput();
+        $this->dispatchBrowserEvent('close-modal');
+    }
+
+    public function deleteCitas(int $dates_id)
+    {
+        $this->dates_id = $dates_id;
+    }
+
+    public function destroyCitas()
+    {
+        Dates::find($this->dates_id)->delete();
+        session()->flash('message', 'Cita Eliminada Correctamente');
+        $this->dispatchBrowserEvent('close-modal');
+    }
+
+    public function closeModal()
+    {
+        $this->resetInput();
+    }
+
+    
+    public function resetInput()
+    {
+        $this->fecha_cita = '';
+        $this->hora_cita = '';
+        $this->descripcion = '';
+        $this->patient_id = '';
+    }
+
+    public function render()
+    {
+        $dates = Dates::where('descripcion','like', '%' . $this->search . '%')->orderBy('id', 'ASC')->paginate(8);
+        $patients = Patient::all();
+        return view('livewire.pages.dates-management', ['dates' => $dates, 'patients' => $patients]); 
+    }
+}
---
+++ resources/views/livewire/modals/citasModal.blade.php
@@ -0,0 +1,148 @@
+<!-- Insert Modal -->
+<div wire:ignore.self class="modal fade" id="citasModal" tabindex="-1" aria-labelledby="citasModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="citasModalLabel">Crear Nueva Cita</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                    wire:click="closeModal"></button>
+            </div>
+            <form wire:submit.prevent="saveCitas">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label>Fecha De La Cita</label>
+                        <input type="date" wire:model.lazy="fecha_cita" class="form-control form-bord">
+                        @error('fecha_cita')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Hora De La Cita</label>
+                        <input type="time" wire:model.lazy="hora_cita" class="form-control form-bord">
+                        @error('hora_cita')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Descripción</label>
+                        <input type="text" wire:model.lazy="descripcion" class="form-control form-bord">
+                        @error('descripcion')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Seleccione Paciente</label>
+                        <select name="sexo" wire:model="patient_id" class="form-control form-bord">
+                            <option value=''>--Selecione Al Paciente--</option>
+                            @foreach($patients as $patient)
+                            <option value={{ $patient->id}}>{{ $patient->nombres . ' ' . $patient->apellidos}}</option>
+                            @endforeach
+                        </select>
+                        @error('patient_id')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" wire:click="closeModal"
+                        data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Update Citas Modal -->
+<div wire:ignore.self class="modal fade" id="updateCitasModal" tabindex="-1"
+    aria-labelledby="updateCitasModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="updateCitasModalLabel">Editar usuario</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" wire:click="closeModal"
+                    aria-label="Close"></button>
+            </div>
+            <form wire:submit.prevent="updateCitas">
+                <div class="modal-body">
+
+                    <div class="mb-3">
+                        <label>Fecha De La Cita</label>
+                        <input type="date" wire:model.lazy="fecha_cita" class="form-control form-bord">
+                        @error('fecha_cita')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Hora De La Cita</label>
+                        <input type="time" wire:model.lazy="hora_cita" class="form-control form-bord">
+                        @error('hora_cita')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Descripción</label>
+                        <input type="text" wire:model.lazy="descripcion" class="form-control form-bord">
+                        @error('descripcion')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
+                        <label>Seleccione Paciente</label>
+                        <select name="sexo" wire:model="patient_id" class="form-control form-bord">
+                            <option value=''>--Selecione Al Paciente--</option>
+                            @foreach($patients as $patient)
+                            <option value={{ $patient->id}}>{{ $patient->nombres . ' ' . $patient->apellidos}}</option>
+                            @endforeach
+                        </select>
+                        @error('patient_id')
+                        <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" wire:click="closeModal"
+                        data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary">Actualizar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- Delete Citas Modal -->
+<div wire:ignore.self class="modal fade" id="deleteCitasModal" tabindex="-1"
+    aria-labelledby="deleteCitasModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteCitasModalLabel">Eliminar Cita</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" wire:click="closeModal"
+                    aria-label="Close"></button>
+            </div>
+            <form wire:submit.prevent="destroyCitas">
+                <div class="modal-body">
+                    <h4>¿Está seguro de que quiere eliminar estos datos?</h4>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" wire:click="closeModal"
+                        data-bs-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Sí. Borrar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
